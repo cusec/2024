4232b07d28dbd2900ae446467656f9ae7ff71efc
Mumtahin Farabi <mfarabi619@gmail.com>
2023-07-18 01:44:59 -0400

    feat: Animated hamburger icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,7 +15,11 @@ export default function Navbar() {
   return (
     <nav className={`bg-black/80 `}>
       {/* Desktop Navbar */}
-      <div className={`max-w-full mx-auto px-6 lg:px-24 uppercase transition ease-out duration-1000 ${isMobileMenuOpen ? 'backdrop-blur-md' : 'backdrop-blur-none'}`}>
+      <div
+        className={`max-w-full mx-auto px-6 lg:px-24 uppercase transition ease-out duration-1000 ${
+          isMobileMenuOpen ? "backdrop-blur-md" : "backdrop-blur-none"
+        }`}
+      >
         <div className="flex items-center justify-between h-16">
           <div className="flex-shrink-0 flex items-center">
             <Link href="/" className="flex items-center">
@@ -62,44 +66,41 @@ export default function Navbar() {
           </div>
 
           {/* Nav Toggle Button */}
+
           <div className="flex md:hidden">
-            <button
-              onClick={toggleMobileMenu}
-              className="inline-flex items-center justify-center p-2 rounded-md text-white hover:text-white hover:scale-125 transition ease-in-out duration-500"
-            >
-              <span className="sr-only">Open mobile menu</span>
-              {/* Hamburger icon */}
-              <svg
-                className={`${isMobileMenuOpen ? "hidden" : "block"} h-8 w-8`}
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-              {/* Close icon */}
-              <svg
-                className={`${isMobileMenuOpen ? "block" : "hidden"} h-8 w-8`}
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
+            <button className="" onClick={toggleMobileMenu}>
+              <div className=" flex overflow-hidden items-center justify-center rounded-full w-[50px] h-[50px] transform transition">
+                <div className="flex flex-col justify-between w-[20px] h-[20px] transform transition-all duration-300 origin-center overflow-hidden">
+                  <div
+                    className={`bg-white h-[2px] w-7 transform transition-all duration-300 origin-left ${
+                      isMobileMenuOpen ? "translate-x-10" : ""
+                    }`}
+                  ></div>
+                  <div
+                    className={`bg-white h-[2px] w-7 rounded transform transition-all duration-700 delay-75 ${
+                      isMobileMenuOpen ? "translate-x-10" : ""
+                    }`}
+                  ></div>
+                  <div
+                    className={`bg-white h-[2px] w-7 transform transition-all duration-700 origin-left delay-150 ${
+                      isMobileMenuOpen ? "translate-x-10" : ""
+                    }`}
+                  ></div>
+
+                  <div className={`absolute items-center justify-between transform transition-all duration-500 top-2.5 flex ${isMobileMenuOpen ?'translate-x-0  w-12': 'translate-x-10 w-0'}`}>
+                    <div
+                      className={`absolute bg-white h-[2px] w-5 transform transition-all duration-700 delay-300 ${
+                        isMobileMenuOpen ? "rotate-45" : "rotate-0"
+                      }`}
+                    ></div>
+                    <div
+                      className={`absolute bg-white h-[2px] w-5 transform transition-all duration-700 delay-300 ${
+                        isMobileMenuOpen ? "-rotate-45" : "rotate-0"
+                      }`}
+                    ></div>
+                  </div>
+                </div>
+              </div>
             </button>
           </div>
         </div>
@@ -111,7 +112,9 @@ export default function Navbar() {
           // `transform top-0 right-0 mt-16 w-full fixed h-full bg-black/80 overflow-auto ease-in-out transition-all duration-300 z-30 ${isMobileMenuOpen ? "translate-y-0" : "translate-y-full"}`
 
           `border-t border-stone-700 transform top-0 right-0 mt-16 w-full fixed h-full bg-black/80 overflow-auto transition duration-1000 ease-in-out z-30 ${
-            isMobileMenuOpen ? "translate-x-0 backdrop-blur-md" : "translate-x-full backdrop-blur-none"
+            isMobileMenuOpen
+              ? "translate-x-0 backdrop-blur-md"
+              : "translate-x-full backdrop-blur-none"
           }`
         }
       >
