aca2225aced78d34978cdce31da5f6d1633dd114
Mumtahin Farabi <mfarabi619@gmail.com>
2023-07-19 08:12:03 -0400

    feat: Large white box overlapped by small corner squares for main section

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,10 +1,11 @@
 const Main = () => {
   return (
-    <main className="mx-6 lg:mx-24 border-4 border-red-950 h-48 relative">
-      <div className="w-[17px] md:w-[21px] aspect-square bg-white border border-blue-600 absolute top-0 left-0"></div>
-      <div className="w-[17px] md:w-[21px] aspect-square bg-white border border-blue-600 absolute top-0 right-0"></div>
-      <div className="w-[17px] md:w-[21px] aspect-square bg-white border border-blue-600 absolute bottom-0 left-0"></div>
-      <div className="w-[17px] md:w-[21px] aspect-square bg-white border border-blue-600 absolute bottom-0 right-0"></div>
+    <main className="mx-6 lg:mx-24 h-48 relative p-[7px] mt-12">
+      <div className="w-[17px] md:w-[21px] aspect-square bg-white border-[2px] border-blue-600 absolute top-0 left-0"></div>
+      <div className="w-[17px] md:w-[21px] aspect-square bg-white border-[2px] border-blue-600 absolute top-0 right-0"></div>
+      <div className="w-[17px] md:w-[21px] aspect-square bg-white border-[2px] border-blue-600 absolute bottom-0 left-0"></div>
+      <div className="w-[17px] md:w-[21px] aspect-square bg-white border-[2px] border-blue-600 absolute bottom-0 right-0"></div>
+      <div className="w-full h-full bg-white border-[5px] border-blue-600"></div>
     </main>
   );
 };
