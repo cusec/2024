42496e709d93763e68ea85202801585e365cadb4
Mumtahin Farabi <mfarabi619@gmail.com>
2023-07-19 08:26:19 -0400

    feat: Add primary dark logo

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import logoPrimaryDark from '../assets/logo-primary-dark.svg'
 const Main = () => {
   return (
     <main className="mx-6 lg:mx-24 h-48 relative p-[7px] mt-12">
@@ -5,7 +7,14 @@ const Main = () => {
       <div className="w-[17px] md:w-[21px] aspect-square bg-white border-[2px] border-blue-600 absolute top-0 right-0"></div>
       <div className="w-[17px] md:w-[21px] aspect-square bg-white border-[2px] border-blue-600 absolute bottom-0 left-0"></div>
       <div className="w-[17px] md:w-[21px] aspect-square bg-white border-[2px] border-blue-600 absolute bottom-0 right-0"></div>
-      <div className="w-full h-full bg-white border-[5px] border-blue-600"></div>
+      <div className="w-full h-full bg-white border-[5px] border-blue-600">
+        <section className="p-8">
+        <Image
+          src={logoPrimaryDark}
+          alt="CUSEC 2024 logo primary dark"
+          />
+          </section>
+      </div>
     </main>
   );
 };
