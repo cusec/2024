84c14be685728b3b14bb9a83a6ff9a2270455561
Mumtahin Farabi <mfarabi619@gmail.com>
2023-07-24 18:38:50 -0400

    feat: Increase navbar opacity

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -40,7 +40,7 @@ export default function Navbar({ clicked }: ChildProps) {
   });
 
   return (
-    <nav className="bg-black/[.80] z-50 sticky top-0">
+    <nav className="bg-black/[.85] z-50 sticky top-0">
       {/* Desktop Navbar */}
       <div
         className={`max-w-full mx-6 lg:mx-24 uppercase transition ease-out duration-1000 ${
