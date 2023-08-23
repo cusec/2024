a91e4a4310799061bb4782157dff6ea03ba43ed3
Mumtahin Farabi <mfarabi619@gmail.com>
2023-08-23 18:32:53 -0400

    feat: Add social media icons to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,23 +1,25 @@
 import { useFadeBackground } from "@/app/layout";
+import {FaFacebookF, FaInstagram, FaLinkedinIn, FaYoutube} from "react-icons/fa"
 
 export default function Footer() {
   const fadeBackground = useFadeBackground();
   return (
     <footer
-      className={`transition-opacity ease-in-out duration-[1500ms] bg-black/[.85] w-full flex justify-center bg-clip-padding${
+      className={`transition-opacity ease-in-out duration-[1500ms] bg-black/[.85] w-full flex justify-center ${
         fadeBackground ? "opacity-0" : "opacity-100 text-red"
       }`}
     >
-      <section className="py-4 space-x-4">
-        <span className="text-white font-[16px]">Privacy</span>
-        <span className="text-white font-[16px]">Code of Conduct</span>
+      <section className="flex items-baseline py-4 space-x-4">
+        <button className="text-white font-[16px]">Privacy</button>
+        <button className="text-white font-[16px]">Code of Conduct</button>
+      <section className="flex items-center text-white space-x-4">
+          <FaFacebookF />
+          <FaInstagram/>
+          <FaLinkedinIn/>
+          <FaYoutube/>
+      </section>
       </section>
 
-      <section>
-        <span className="">
-          
-        </span>
-      </section>
     </footer>
   );
 }
