82d603de9ddc2b2cb680a63b44274e6cb70d6150
Mumtahin Farabi <mfarabi619@gmail.com>
2023-08-24 19:38:40 -0400

    feat: Add easter egg to email page

--- src/app/email/page.tsx
+++ src/app/email/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Image from "next/image";
 
 // Importing images
@@ -140,6 +141,11 @@ export default function email_signature() {
             </tr>
           </tbody>
         </table>
+      <section className="mt-20 font-bold hover:text-blue-600 hover:text-5xl transition ease-in-out duration-100 select-none">
+        <Link href={"https://www.youtube.com/watch?v=xvFZjo5PgG0"}>
+          CLICK HERE FOR UNLIMITED RAM AND FREE ROBUX 🥳💃
+        </Link>
+      </section>
       </div>
     </>
   );
