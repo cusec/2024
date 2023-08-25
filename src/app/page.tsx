29cc9c0b485b53f01178eeb8f74e01a2f2cf3646
Mumtahin Farabi <mfarabi619@gmail.com>
2023-08-25 02:37:02 -0400

    feat: Wrap FAQ section in gradient background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,31 +19,34 @@ export default function Home() {
   const fadeBackground = useFadeBackground();
   return (
     <>
-  <Head>
-    <title>CUSEC 2024 Home</title>
-    <meta name = "description" content="Official website for the 2024 edition of the Canadian University Software Engineering Conference."></meta>
-  </Head>
-    <div className="w-full">
-      <div
-        className={`${
-          fadeBackground ? "gradientBackground" : "gradientGridBackground"
-        } flex items-center justify-center`}
-      >
-        <Landingbox />
-      </div>
+      <Head>
+        <title>CUSEC 2024 Home</title>
+        <meta
+          name="description"
+          content="Official website for the 2024 edition of the Canadian University Software Engineering Conference."
+        ></meta>
+      </Head>
+      <div className="w-full">
+        <div
+          className={`${
+            fadeBackground ? "gradientBackground" : "gradientGridBackground"
+          } flex items-center justify-center`}
+        >
+          <Landingbox />
+        </div>
 
-      <div className="flex flex-col items-center">
-      <div className="mx-6 lg:mx-24 max-w-screen-lg lg:w-full">
-      <About /> 
+        <div className="flex flex-col items-center">
+          <div className="mx-6 lg:mx-24 max-w-screen-lg lg:w-full">
+            <About />
+          </div>
+          <div className="w-full gradientBackground">
+            <Faq />
+            <div className="mx-6 lg:mx-24 pb-10 max-w-screen-lg lg:w-full">
+              <Contact />
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="mx-6 lg:mx-24 pb-10 max-w-screen-lg lg:w-full">
-      <Faq/>
-      </div>
-      <div className="mx-6 lg:mx-24 pb-10 max-w-screen-lg lg:w-full">
-      <Contact/>
-      </div>
-      </div>
-    </div>
-          </>
+    </>
   );
 }
