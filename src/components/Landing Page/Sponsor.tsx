27a309b631693979bdb51df11371da055627db77
Mumtahin Farabi <mfarabi619@gmail.com>
2023-08-25 10:33:04 -0400

    feat: Complete desktop layout for sponsor section

--- src/components/Landing Page/Sponsor.tsx
+++ src/components/Landing Page/Sponsor.tsx
@@ -10,25 +10,33 @@ export default function Sponsor() {
       }`}
     >
       <div className="bg-black bg-opacity-60 shadow mx-auto px-20 text-white">
-        <h2 className="text-[28px] text-center font-semibold py-14 px-10">
-          Sponsor CUSEC 2024
-        </h2>
-
-        <div className="relative max-w-md aspect-square mx-auto">
-          <div className="absolute top-[-17px] right-[-18px] bg-gradient-to-br from-royalPurple via-roseQuartz to-goldenApricot w-full h-full scale-[95%]">
-            {/* This div is for the background gradient */}
+        <div className="grid grid-cols-1 md:grid-cols-2 items-start py-14 md:py-32 max-w-6xl mx-auto">
+          <h2 className="text-[28px] text-center font-semibold pb-10 block md:hidden">
+            Sponsor CUSEC 2024
+          </h2>
+          <div className="relative max-w-md aspect-square mx-auto">
+            <div className="absolute top-[-17px] right-[-18px] bg-gradient-to-br from-royalPurple via-roseQuartz to-goldenApricot w-full h-full scale-[95%]">
+              {/* This div is for the background gradient */}
+            </div>
+            <BlueBorderSquareBox>
+              <div className="p-24 aspect-square">Some content here</div>
+            </BlueBorderSquareBox>
           </div>
-          <BlueBorderSquareBox>
-            <div className="p-24 aspect-square">Some content here</div>
-          </BlueBorderSquareBox>
-        </div>
 
-        <p className="font-regular text-center pt-10 pb-6 max-w-sm mx-auto">
-          Over the last 21 years, CUSEC has collaborated with an extensive array
-          of industry leaders. These partnerships have helped make CUSEC
-          possible while providing students invaluable opportunities to create
-          lasting connections with seasoned professionals.
-        </p>
+          <div className="text-center md:text-right mx-auto md:pl-10">
+          <h2 className="text-[28px] md:text-[40px] font-semibold hidden md:block">
+            Sponsor CUSEC 2024
+          </h2>
+          <p className="md:text-[20px] font-regular pt-10 pb-6">
+            Over the last 21 years, CUSEC has collaborated with an extensive
+            array of industry leaders. These partnerships have helped make CUSEC
+            possible while providing students invaluable opportunities to create
+            lasting connections with seasoned professionals.
+          </p>
+          
+          </div>
+
+        </div>
       </div>
     </div>
   );
