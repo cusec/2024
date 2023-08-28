273600f7e4b0368899a5bb1c558bf8e86743179a
Mumtahin Farabi <mfarabi619@gmail.com>
2023-08-27 20:02:41 -0400

    feat: Give BlueBorderSquareBox component a backdrop blur

--- src/components/Landing Page/BlueBorderSquareBox.tsx
+++ src/components/Landing Page/BlueBorderSquareBox.tsx
@@ -7,11 +7,11 @@ export default function BlueBorderSquareBox({
 }: BlueBorderSquareBoxProps) {
   return (
     <div className="relative p-[7px]">
-      <div className="w-[17px] md:w-[21px] aspect-square bg-white border-[2px] border-blue-600 absolute top-0 left-0"></div>
-      <div className="w-[17px] md:w-[21px] aspect-square bg-white border-[2px] border-blue-600 absolute top-0 right-0"></div>
-      <div className="w-[17px] md:w-[21px] aspect-square bg-white border-[2px] border-blue-600 absolute bottom-0 left-0"></div>
-      <div className="w-[17px] md:w-[21px] aspect-square bg-white border-[2px] border-blue-600 absolute bottom-0 right-0"></div>
-      <div className="w-full h-full bg-white bg-opacity-70 border-[5px] border-blue-600">
+      <div className="w-[17px] md:w-[21px] aspect-square bg-white border-[2px] border-blue-600 absolute top-0 left-0 z-10"></div>
+      <div className="w-[17px] md:w-[21px] aspect-square bg-white border-[2px] border-blue-600 absolute top-0 right-0 z-10"></div>
+      <div className="w-[17px] md:w-[21px] aspect-square bg-white border-[2px] border-blue-600 absolute bottom-0 left-0 z-10"></div>
+      <div className="w-[17px] md:w-[21px] aspect-square bg-white border-[2px] border-blue-600 absolute bottom-0 right-0 z-10"></div>
+      <div className="w-full h-full bg-white bg-opacity-70 border-[5px] border-blue-600 backdrop-blur-[2px]">
         {children}
       </div>
     </div>
