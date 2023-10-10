69016d24d6d65dd2102d589970e7af8cbc629f05
Mumtahin Farabi <mfarabi619@gmail.com>
2023-10-09 22:47:04 -0400

    feat: Create 'Become a Sponsor' section on Sponsors page

--- src/app/sponsors/page.tsx
+++ src/app/sponsors/page.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import Head from "next/head";
 import Fade from "@/components/Fade";
 import Image from "next/image";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 import Main from "@/app/sponsors/Main";
 import CusecSponsors from "@/app/sponsors/CusecSponsors";
@@ -19,8 +20,6 @@ export default function Sponsors() {
           content="Official website for the 2024 edition of the Canadian University Software Engineering Conference."
         />
       </Head>
-
-      {/* TODO: Fix gradient lines not showing up */}
 
       <div className="w-full">
         {/* Top Left Gradient Lines */}
@@ -70,6 +69,24 @@ export default function Sponsors() {
           <div className="mx-6 lg:mx-24 max-w-screen-lg lg:w-full md:-mt-3 mb-16">
             <Fade>
               <WhySponsor />
+            </Fade>
+          </div>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <div
+            className="w-full gradientBackground"
+            style={{
+              boxShadow:
+                "0px 3px 8px 4px rgba(0, 0, 0, 0.5), 0px -3px 8px 4px rgba(0, 0, 0, 0.5)",
+            }}
+          >
+            <Fade>
+              <div className="bg-black bg-opacity-60 px-8 flex flex-col justify-center items-center text-center py-48 space-y-6 md:space-y-8">
+                <p className="text-white text-[16px] md:text-[26px]">Want to help spark inspiration into tech-savvy minds?</p>
+                <p className="font-semibold text-[20px] md:text-[40px] w-fit text-transparent bg-clip-text bg-gradient-to-r from-royalPurple via-roseQuartz to-goldenApricot">Become a 2024 Sponsor</p>
+                <Link href="/" className="drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] bg-[#AD65E3] px-3 min-[390px]:px-10 md:px-20 py-1 text-center rounded-full uppercase text-[14px] md:text-[18px] font-semibold text-white tracking-wide transition ease-in-out duration-500 hover:scale-110 hover:bg-goldenApricot hover:text-white">Sign Up</Link>
+              </div>
             </Fade>
           </div>
         </div>
