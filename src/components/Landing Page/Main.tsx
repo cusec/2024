262531bbe3c042ebf9ebdf85b6076b6100959fe4
Mumtahin Farabi <mfarabi619@gmail.com>
2023-10-14 14:07:41 -0400

    refactor: Landing page main section

--- src/components/Landing Page/Main.tsx
+++ src/components/Landing Page/Main.tsx
@@ -5,27 +5,32 @@ import greyEllipse from "src/assets/grey-ellipse.svg";
 import BlueBorderSquareBox from "./BlueBorderSquareBox";
 import { motion } from "framer-motion";
 
-export default function Landingbox (){
+const fadeInConfig = {
+  initial: { opacity: 0, scale: 0.8 },
+  whileInView: { opacity: 1, scale: 1 },
+  viewport: { once: true },
+};
+
+const transitionConfig = (delay = 0) => ({
+  duration: 1.5,
+  delay: delay,
+  type: "spring",
+  bounce: 0.5,
+});
+
+const speakerSignUpLink =
+  "https://docs.google.com/forms/d/e/1FAIpQLSekk6WN5YEMg-tRza9SbMTIar_mHFEUQ9jjLIgw8eF3YNmIog/viewform";
+const sponsorEmailLink =
+  "mailto:sponsor@cusec.net?subject=CUSEC 2024 Sponsorship Information Request";
+const locationLink =
+  "https://www.google.com/maps/place/Le+Centre+Sheraton+Montreal+Hotel/@45.4965835,-73.5735774,15.5z";
+
+const Landingbox = () => {
   return (
-    <motion.main
-      initial={{ opacity: 0, scale: 0.8 }}
-      whileInView={{ opacity: 1, scale: 1 }}
-      transition={{ duration: 1, type: "spring", bounce: 0.5 }}
-      viewport={{ once: true }}
-    >
+    <motion.main {...fadeInConfig} transition={transitionConfig()}>
       <BlueBorderSquareBox>
         <section className="py-8 px-8 text-center space-y-5 flex flex-col justify-center items-center">
-          <motion.div
-            initial={{ opacity: 0, scale: 0.8 }}
-            whileInView={{ opacity: 1, scale: 1 }}
-            transition={{
-              duration: 1,
-              delay: 0.3,
-              type: "spring",
-              bounce: 0.5,
-            }}
-            viewport={{ once: true }}
-          >
+          <motion.div {...fadeInConfig} transition={transitionConfig(0.3)}>
             <Image
               src={logoPrimaryDark}
               alt="CUSEC 2024 logo primary dark"
@@ -35,29 +40,15 @@ export default function Landingbox (){
             />
           </motion.div>
           <motion.p
-            initial={{ opacity: 0, scale: 0.8 }}
-            whileInView={{ opacity: 1, scale: 1 }}
-            transition={{
-              duration: 1,
-              delay: 0.6,
-              type: "spring",
-              bounce: 0.5,
-            }}
-            viewport={{ once: true }}
+            {...fadeInConfig}
+            transition={transitionConfig(0.6)}
             className="font-semibold text-[18px] leading-[25px] tracking-wider"
           >
             Canadian University Software Engineering Conference
           </motion.p>
           <motion.p
-            initial={{ opacity: 0, scale: 0.8 }}
-            whileInView={{ opacity: 1, scale: 1 }}
-            transition={{
-              duration: 1,
-              delay: 0.9,
-              type: "spring",
-              bounce: 0.5,
-            }}
-            viewport={{ once: true }}
+            {...fadeInConfig}
+            transition={transitionConfig(0.9)}
             className="text-[15px] text-[#615757]"
           >
             January 11-13
@@ -67,9 +58,7 @@ export default function Landingbox (){
               className="inline-block mx-3"
             />
             <Link
-              href={
-                "https://www.google.com/maps/place/Le+Centre+Sheraton+Montreal+Hotel/@45.4965835,-73.5735774,15.5z/data=!4m9!3m8!1s0x4cc91a42f46cd423:0xce6742b1fed51b6f!5m2!4m1!1i2!8m2!3d45.4979625!4d-73.5716361!16zL20vMDhrbnJm?entry=ttu"
-              }
+              href={locationLink}
               target="_blank"
               className="hover:text-blue-500 transition ease-in-out duration-500"
             >
@@ -78,57 +67,48 @@ export default function Landingbox (){
           </motion.p>
 
           <div className="flex flex-col md:flex-row space-y-4 md:space-y-0">
-            <motion.a
-              initial={{ opacity: 0 }}
-              whileInView={{ opacity: 1 }}
-              transition={{
-                duration: 1,
-                delay: 1.2,
-                type: "ease-in-out",
-                bounce: 0.5,
-              }}
-              viewport={{ once: true }}
-              href={
-                "https://docs.google.com/forms/d/e/1FAIpQLSekk6WN5YEMg-tRza9SbMTIar_mHFEUQ9jjLIgw8eF3YNmIog/viewform"
-              }
-              target="_blank"
-              className="drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] bg-[#AD65E3] px-3 min-[390px]:px-5 md:px-20 py-3 text-center rounded-full uppercase text-[14px] md:text-[18px] font-semibold text-white tracking-wide transition ease-in-out duration-500 hover:scale-110 hover:bg-goldenApricot hover:text-white"
+            <motion.div
+              {...fadeInConfig}
+              transition={transitionConfig(1.2)}
+              className="flex self-center"
             >
-              Become a Speaker
-            </motion.a>
-            <motion.a
-              initial={{ opacity: 0 }}
-              whileInView={{ opacity: 1 }}
-              transition={{
-                duration: 1,
-                delay: 1.4,
-                type: "ease-in-out",
-                bounce: 0.5,
-              }}
-              viewport={{ once: true }}
-              href="mailto:sponsor@cusec.net?subject=CUSEC 2024 Sponsorship Information Request"
-              className="md:hidden block text-royalBlue font-semibold underline underline-offset-2 decoration-2"
+              <a
+                href={speakerSignUpLink}
+                target="_blank"
+                className="drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] bg-[#AD65E3] px-3 min-[390px]:px-5 md:px-20 py-3 text-center rounded-full uppercase text-[14px] md:text-[18px] font-semibold text-white tracking-wide transition ease-in-out duration-500 hover:scale-110 hover:bg-goldenApricot hover:text-white"
+              >
+                Become a Speaker
+              </a>
+            </motion.div>
+            <motion.div
+              {...fadeInConfig}
+              transition={transitionConfig(1.4)}
+              className="flex"
             >
-              Interested in Sponsoring?
-            </motion.a>
-            <motion.a
-              initial={{ opacity: 0 }}
-              whileInView={{ opacity: 1 }}
-              transition={{
-                duration: 1,
-                delay: 1.4,
-                type: "ease-in-out",
-                bounce: 0.5,
-              }}
-              viewport={{ once: true }}
-              href="mailto:sponsor@cusec.net?subject=CUSEC 2024 Sponsorship Information Request"
-              className="drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] hidden md:inline bg-[#FDF3FF] border-2 border-royalBlue ml-9 px-[85px] py-3 text-center rounded-full uppercase text-[18px] font-semibold text-royalBlue tracking-wide transition ease-in-out duration-500 hover:scale-110 hover:bg-royalBlue hover:text-white "
+              <a
+                href={sponsorEmailLink}
+                className="flex md:hidden text-royalBlue font-semibold underline underline-offset-2 decoration-2 transition ease-in-out duration-500 hover:scale-105"
+              >
+                Interested in Sponsoring?
+              </a>
+            </motion.div>
+            <motion.div
+              {...fadeInConfig}
+              transition={transitionConfig(1.4)}
+              className="flex"
             >
-              Sponsor
-            </motion.a>
+              <a
+                href={sponsorEmailLink}
+                className="drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] hidden md:inline bg-[#FDF3FF] border-2 border-royalBlue ml-9 px-[85px] py-3 text-center rounded-full uppercase text-[18px] font-semibold text-royalBlue tracking-wide transition ease-in-out duration-500 hover:scale-110 hover:bg-royalBlue hover:text-white "
+              >
+                Sponsor
+              </a>
+            </motion.div>
           </div>
         </section>
       </BlueBorderSquareBox>
     </motion.main>
   );
 };
+
+export default Landingbox;
