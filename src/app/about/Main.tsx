22cf0276ce8636acbda3a7095843237eaa1de7c6
Mumtahin Farabi <mfarabi619@gmail.com>
2023-10-14 21:28:37 -0400

    feat: Add animation to main section of About Us page

--- src/app/about/Main.tsx
+++ src/app/about/Main.tsx
@@ -1,12 +1,41 @@
 import Image from "next/image";
 import cusec_2023_group_photo from "./cusec_2023_group_photo.png";
+import { motion } from "framer-motion";
 
 interface MainProps {
   isMediumScreen: boolean;
 }
 
-export default function Main({ isMediumScreen }: MainProps) {
+const animateInConfig = {
+  initial: { opacity: 0, x: 100 },
+  whileInView: { opacity: 1, x: 0 },
+  viewport: { once: true },
+};
 
+const transitionConfig = (delay = 0) => ({
+  duration: 1.5,
+  delay: delay,
+  type: "spring",
+  bounce: 0.5,
+});
+
+const {
+  white,
+  purple400,
+  roseQuartz,
+  goldenApricot,
+  getMotionProps = (color: string) => ({
+    initial: { color: white },
+    animate: { color },
+  }),
+} = {
+  white: "#ffffff",
+  purple400: "#c084fc",
+  roseQuartz: "#E087BB",
+  goldenApricot: "#FFB963",
+};
+
+export default function Main({ isMediumScreen }: MainProps) {
   return (
     <div className="relative min-h-[96vh] w-[calc(100vw-10px)] translate-y-6 md:-translate-x-[1vw]">
       <svg width="0" height="0">
@@ -42,17 +71,39 @@ export default function Main({ isMediumScreen }: MainProps) {
           objectPosition="center"
           alt="CUSEC 2023 Group Photo"
         />
+
         <div className="absolute inset-0 z-1 flex justify-center items-center lg:mx-1">
           {/* Text */}
-          <h1 className="uppercase text-white font-semibold text-[24px] ml-10 sm:mr-24 lg:ml-24 lg:mr-52 lg:text-[50px]">
+          <motion.h1
+            {...animateInConfig}
+            transition={transitionConfig(0.2)}
+            className="uppercase text-white font-semibold text-[24px] ml-10 sm:mr-24 lg:ml-24 lg:mr-52 lg:text-[50px]"
+          >
             Fueling a journey of{" "}
-            <span className="text-purple-400">knowledge</span>,
+            <motion.span
+              {...getMotionProps(purple400)}
+              transition={transitionConfig(0.7)}
+            >
+              knowledge
+            </motion.span>
+            ,
             <br className="hidden lg:block" />{" "}
-            <span className="text-roseQuartz">inspiration</span>, &{" "}
-            <span className="text-goldenApricot">opportunity</span> for students{" "}
-            <br className="hidden lg:block" />
+            <motion.span
+              {...getMotionProps(roseQuartz)}
+              transition={transitionConfig(0.9)}
+            >
+              inspiration
+            </motion.span>
+            , &{" "}
+            <motion.span
+              {...getMotionProps(goldenApricot)}
+              transition={transitionConfig(1.2)}
+            >
+              opportunity
+            </motion.span>{" "}
+            for students <br className="hidden 2xl:block" />
             across Canada
-          </h1>
+          </motion.h1>
         </div>
       </div>
     </div>
