6a5461dd838fd4caa212232ab8885593327804a8
Mumtahin Farabi <mfarabi619@gmail.com>
2023-10-16 00:54:15 -0400

    fix: Why Join section sign up button linking to home page

--- src/app/about/WhyJoin.tsx
+++ src/app/about/WhyJoin.tsx
@@ -134,11 +134,12 @@ export default function WhyJoin() {
           Be a part of our 2024 event this January!
         </p>
         <p className="font-semibold text-[20px] md:text-[36px] text-transparent bg-clip-text bg-gradient-to-r from-royalPurple via-roseQuartz to-goldenApricot w-fit">
-          Register Now⬇
+          Register Now
         </p>
         <Link
           className={`drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] bg-royalBlue hover:bg-goldenApricot mx-auto px-5 md:px-10 py-1 mt-3 md:mt-6 text-center rounded-full uppercase md:text-[18px] font-semibold text-white tracking-wide transition ease-in-out duration-500 hover:scale-110 hover:text-white`}
-          href={`/`}
+          href={`https://www.tickettailor.com/events/cusec/943944`}
+          target="_blank"
         >
           Sign Up
         </Link>
