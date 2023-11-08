fe8b74624e609bf006d2b45a9401f343693b2ab6
Mumtahin Farabi <mfarabi619@gmail.com>
2023-11-08 12:39:36 -0500

    chore: Add sitemap generation script for improved SEO

---
+++ scripts/generate-sitemap.ts
@@ -0,0 +1,42 @@
+const { SitemapStream, streamToPromise } = require('sitemap');
+const { createWriteStream } = require('fs');
+const { resolve } = require('path');
+
+// Define pages
+const pages = [
+  { url: '/', changefreq: 'daily', priority: 0.7 },
+  { url: '/about', changefreq: 'daily', priority: 0.5 },
+  { url: '/sponsors', changefreq: 'daily', priority: 0.5 },
+];
+
+(async () => {
+  // Create a stream to write to with hostname
+  const sitemapStream = new SitemapStream({ hostname: 'https://2024.cusec.net' });
+
+  // Create a write stream to write the sitemap to a file
+  const sitemapOutput = createWriteStream(resolve(__dirname, '../public/sitemap.xml'));
+  sitemapStream.pipe(sitemapOutput);
+
+  // Write each URL to the stream
+  pages.forEach(page => {
+    sitemapStream.write(page);
+  });
+
+  // Listen for any errors on the stream
+  sitemapStream.on('error', (e: any) => {
+    console.error(e);
+    // Handle error
+  });
+
+  // Convert our stream to a promise
+  const sitemapPromise = streamToPromise(sitemapStream);
+
+  // End the stream
+  sitemapStream.end();
+
+  // Wait for the stream to finish and output the sitemap
+  sitemapPromise.then((sitemap: Buffer) => console.log(sitemap.toString())).catch((e: any) => {
+    console.error(e);
+    // Handle error
+  });
+})();
