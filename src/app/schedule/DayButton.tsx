4a36d0373ee649aa14af7574831a30a0f06454b7
Mumtahin Farabi <mfarabi619@gmail.com>
2024-01-05 06:08:37 -0500

    feat: Render schedule from json

--- src/app/schedule/DayButton.tsx
+++ src/app/schedule/DayButton.tsx
@@ -26,7 +26,7 @@ return (
 
       {/* Pill Button with Day and Date */}
       <p
-        className={`flex flex-col items-center justify-center rounded-t-full rounded-b-full font-medium py-3 px-8 md:px-10 max-w-[60px] ${bgColor} transition duration-300`}
+        className={`flex flex-col items-center justify-center rounded-t-full rounded-b-full font-medium py-3 px-8 md:px-10 max-w-[60px] ${bgColor} transition-all ease-in-out duration-1000`}
       >
         <span className={`text-[20px] md:text-[24px] ${textColor} transition-all ease-in-out duration-1000`}>
           {props.Day}
@@ -41,35 +41,3 @@ return (
     </section>
   );
 }
-
-
-//   // A solid fallback background color similar to the gradient's end color
-//   const fallbackBgColor = props.selected ? 'bg-roseQuartz' : 'bg-gray-200'; 
-
-//   return (
-//     <section
-//       className={`relative flex flex-col justify-center items-center rounded-lg hover:cursor-pointer transition duration-[2000ms] ${fallbackBgColor}`}
-//       onClick={props.onDayButtonClick}
-//     >
-//       {/* Overlay for smooth transition (Initially fully transparent) */}
-//       <div className={`absolute inset-0 rounded-lg transition-opacity duration-[2000ms] ${props.selected ? 'opacity-100 bg-gradient-to-br from-royalPurple via-roseQuartz to-goldenApricot' : 'opacity-0'}`}></div>
-
-//       {/* Text for day of the conference, for example: 'Day One' */}
-//       <span className={`relative z-10 text-[16px] md:text-[24px] font-semibold text-gray-600 transition duration-[2000ms]`}>
-//         {props.titleText}
-//       </span>
-
-//       {/* Pill Button with Day and Date */}
-//       <p className="relative z-10 flex flex-col items-center justify-center rounded-t-full rounded-b-full font-medium py-3 px-8 md:px-10 max-w-[60px] transition duration-[2000ms]">
-//         <span className={`text-[20px] md:text-[24px] ${props.selected ? 'text-white' : 'text-black'} transition duration-[2000ms]`}>
-//           {props.Day}
-//         </span>
-//         <span className={`text-[24px] md:text-[32px] -mt-1 mb-1 ${props.selected ? 'text-white' : 'text-black'} transition duration-[2000ms]`}>
-//           {props.Date}
-//         </span>
-
-//         {/* Little dot that shows the user whether the pill is selected or not */}
-//         <span className={`h-2 w-2 rounded-full ${props.selected ? 'bg-white' : 'bg-transparent'} transition duration-[2000ms]`}></span>
-//       </p>
-//     </section>
-//   );
--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -4,6 +4,7 @@ import Fade from "@/components/Fade";
 import Image from "next/image";
 import { motion } from "framer-motion";
 import DayButton from "./DayButton";
+import schedule from "./schedule.json";
 
 export default function Schedule() {
   // Assume the identifier is the titleText for simplicity, you can use more unique identifiers if necessary
@@ -13,8 +14,14 @@ export default function Schedule() {
     setSelectedDay(day);
   };
 
+  const colors = [
+    "border-royalPurple",
+    "border-roseQuartz",
+    "border-goldenApricot",
+  ];
+
   return (
-    <>
+    <div className="bg-pink-100">
       <title>Schedule - CUSEC 2024</title>
       <meta
         name="description"
@@ -23,13 +30,16 @@ export default function Schedule() {
       <div className="w-full flex justify-center">
         <div className="w-full max-w-screen-2xl mx-6 lg:mx-24 overflow-hidden mt-20">
           <Fade>
+            {/* Title */}
             <motion.span className="flex justify-center">
               <h1 className="text-center text-[28px] md:text-[40px] font-semibold tracking-tight w-fit">
                 January 2024 Schedule
                 <hr className="max-w-[3rem] md:max-w-[4rem] h-2 bg-pink-300 mb-2 ml-[9rem] md:ml-[13rem]" />
               </h1>
             </motion.span>
-            <div className="my-10 rounded-lg bg-gradient-to-r from-royalPurple via-roseQuartz to-goldenApricot p-[1px] shadow-[0_0px_8px_rgba(0,0,0,0.25)]">
+
+            {/* Button to select the day */}
+            <motion.div className="my-10 rounded-lg bg-gradient-to-r from-royalPurple via-roseQuartz to-goldenApricot p-[2px] shadow-[0_0px_8px_rgba(0,0,0,0.5)]">
               <div className="flex bg-white justify-evenly rounded-lg py-5 px-6">
                 <DayButton
                   titleText="Day One"
@@ -55,10 +65,131 @@ export default function Schedule() {
                   onDayButtonClick={() => handleDayButtonClick("Day Three")}
                 />
               </div>
-            </div>
+            </motion.div>
+
+            {/* Schedule of events */}
+            <motion.div className="my-10 rounded-lg bg-gradient-to-r from-royalPurple via-roseQuartz to-goldenApricot p-[2px] shadow-[0_0px_8px_rgba(0,0,0,0.5)]">
+              <div className="bg-white rounded-lg py-5 px-6">
+                <h2 className="text-[16px] md:text-[30px]">
+                  <span className="font-semibold">{schedule[0]["Date"]}, </span>{" "}
+                  <span>{schedule[0]["Day"]}</span>
+                </h2>
+                {schedule[0].items.map((item, index) =>
+                  (() => {
+                    // Determine the color class for the border based on the current index
+                        const borderColorClass = colors[index % colors.length]; // Cycle through colors
+                        
+                    switch (item.category) {
+                      // Category 1 events
+                      case "1":
+                        return (
+                          <div key={index} className="m-1 flex">
+                            {/* Time column */}
+                                <div className={`flex flex-col pr-4 border-r-2 ${borderColorClass}`}>
+                              <span>{item.start_time}</span>
+                              <span>{item.end_time}</span>
+                            </div>
+
+                            {/* Event info column */}
+                            <div className="flex flex-col ml-4">
+                              <span className="font-semibold">
+                                {item.title}
+                              </span>
+                              <span className="text-zinc-400 text-[12px]">
+                                {item.location}
+                              </span>
+                            </div>
+                          </div>
+                        );
+
+                      //   Category 2 events
+                      case "2":
+                        return (
+                          <div key={index} className="m-1 flex">
+                            {/* Time column */}
+                            <div className={`flex flex-col pr-4 border-r-2 ${borderColorClass}`}>
+                              <span>{item.start_time}</span>
+                              <span>{item.end_time}</span>
+                            </div>
+
+                            {/* Event Info column */}
+                            <div className="flex flex-col ml-4">
+                              <span className="font-semibold">
+                                {item.speaker}
+                              </span>
+                              <span className="text-zinc-500">
+                                {item.title}
+                              </span>
+                              <span className="text-zinc-400 text-[12px]">
+                                {item.location}
+                              </span>
+                            </div>
+                          </div>
+                        );
+
+                      //Category 3 events
+                      case "3":
+                        return (
+                          <div
+                            key={index}
+                            className="m-1 flex"
+                          >
+                            {/* Time column */}
+                            <div className={`flex flex-col pr-4 border-r-2 ${borderColorClass}`}>
+                              <span>{item.start_time}</span>
+                              <span>{item.end_time}</span>
+                            </div>
+
+                            {/* Events container */}
+                            <div className="flex ml-4">
+                              {/* First event */}
+                              <div className="flex flex-col pr-4">
+                                <span className="font-semibold">
+                                  {item.items &&
+                                    item.items.length > 0 &&
+                                    item.items[0].title}
+                                </span>
+                                <span className="text-zinc-500 text-[12px]">
+                                  {item.items &&
+                                    item.items.length > 0 &&
+                                    item.items[0].location}
+                                </span>
+                              </div>
+                              {/* Second event */}
+                              <div className="flex flex-col pl-4 border-l-2 border-royalBlue">
+                                <span className="font-semibold">
+                                  {item.items &&
+                                    item.items.length > 0 &&
+                                    item.items[1].title}
+                                </span>
+                                <span className="text-zinc-500 text-[12px]">
+                                  {item.items &&
+                                    item.items.length > 0 &&
+                                    item.items[1].location}
+                                </span>
+                              </div>
+                              <span className="font-semibold">
+                                {item.title}
+                              </span>
+                              <span className="text-zinc-500 text-[12px]">
+                                {item.location}
+                              </span>
+                            </div>
+                          </div>
+                        );
+                      // Category 4 events
+                      case "4":
+                        return <div>Category 4 Component</div>;
+                      default:
+                        return null; // Or some default component for other categories
+                    }
+                  })()
+                )}
+              </div>
+            </motion.div>
           </Fade>
         </div>
       </div>
-    </>
+    </div>
   );
 }
