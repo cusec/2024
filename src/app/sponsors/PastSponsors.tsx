1d92b2aac0860315103b1c574892434ef9ddcdac
Mumtahin Farabi <mfarabi619@gmail.com>
2024-01-06 14:09:54 -0500

    chore: Remove dead code from PastSponsors.tsx

--- src/app/sponsors/PastSponsors.tsx
+++ src/app/sponsors/PastSponsors.tsx
@@ -25,59 +25,6 @@ const transitionConfig = (delay = 0) => ({
 });
 
 export default function PastSponsors() {
-  // const sponsorLogos = [
-  //   {
-  //     src: "/images/sponsor logos/rbc_logo.svg",
-  //     alt: "RBC Logo",
-  //     styles: "w-[70px] h-[90px] md:w-[200px] md:h-[260px]",
-  //   },
-  //   {
-  //     src: "/images/sponsor logos/d2l_logo.svg",
-  //     alt: "D2L Logo",
-  //     styles: "w-[57px] h-[24px] md:w-[165px] md:h-[70px]",
-  //   },
-  //   {
-  //     src: "/images/sponsor logos/accenture_logo.svg",
-  //     alt: "Accenture Logo",
-  //     styles: "w-[79px] h-[20px] md:w-[230px] md:h-[60px]",
-  //   },
-  //   {
-  //     src: "/images/sponsor logos/unity_logo.svg",
-  //     alt: "Unity Logo",
-  //     styles: "w-[114px] h-[43px] md:w-[337px] md:h-[125px]",
-  //   },
-  //   {
-  //     src: "/images/sponsor logos/csc_logo.svg",
-  //     alt: "CSC Logo",
-  //     styles: "w-[45px] h-[45px] md:w-[130px] md:h-[130px] md:left-20",
-  //   },
-  //   {
-  //     src: "/images/sponsor logos/morgan_stanley_logo.svg",
-  //     alt: "Morgan Stanley Logo",
-  //     styles: "w-[114px] h-[18px] md:w-[330px] md:h-[50px]",
-  //   },
-  //   {
-  //     src: "/images/sponsor logos/nokia_logo.svg",
-  //     alt: "Nokia Logo",
-  //     styles: "w-[80px] h-[14px] md:w-[235px] md:h-[40px]",
-  //   },
-  //   {
-  //     src: "/images/sponsor logos/stickermule_logo.svg",
-  //     alt: "Sticker Mule Logo",
-  //     styles: "w-[97px] h-[56px] md:w-[285px] md:h-[165px]",
-  //   },
-  //   {
-  //     src: "/images/sponsor logos/deloitte_logo.svg",
-  //     alt: "Deloitte Logo",
-  //     styles: "w-[98px] h-[22px] md:w-[288px] md:h-[55px]",
-  //   },
-  //   {
-  //     src: "/images/sponsor logos/fellow_logo.svg",
-  //     alt: "Fellow Logo",
-  //     styles: "w-[112px] h-[30px] md:w-[330px] md:h-[88px]",
-  //   },
-  // ];
-
   return (
     <div className="relative">
       <motion.div
@@ -106,20 +53,6 @@ export default function PastSponsors() {
           transition={transitionConfig(0.2)}
           className="block max-w-[2.5rem] md:max-w-[3.75rem] h-2 bg-roseQuartz mb-10"
         />
-        {/* <motion.p
-          {...animateInConfigText}
-          transition={transitionConfig(0.4)}
-          className="font-regular text-[15px] md:text-[20px]"
-        >
-          Each year our sponsors play a crucial role in making CUSEC an exciting
-          and enriching experience for tech-savvy students. Their generous
-          support fuels the conference&apos;s activities and enhances the
-          overall atmosphere for students and professionals to connect, learn,
-          and explore new horizons in software engineering. Our sponsors are
-          essential in keeping the conference dynamic and full of opportunities,
-          ensuring that CUSEC continues to be a must-attend event for those
-          passionate about technology and innovation.
-        </motion.p> */}
       </section>
       <motion.div
         {...animateInConfigImage}
@@ -148,24 +81,12 @@ export default function PastSponsors() {
         ></motion.div>
 
         <BlueBorderSquareBox>
-          {/* <div className="h-[257px] md:h-[500px] md:gap-8 py-5 px-8 md:py-10 md:px-12 relative"> */}
-          {/* {sponsorLogos.map((logo) => (
-              <div key={logo.src} className={`absolute ${logo.styles}`}>
-                <Image
-                  src={logo.src}
-                  alt={logo.alt}
-                  layout="fill"
-                  objectFit="cover"
-                />
-              </div>
-            ))} */}
           <motion.div
             {...animateInConfigImage}
             transition={transitionConfig(0.9)}
             className="aspect-square flex flex-col justify-center items-center relative right-4"
           >
             <Image
-              // src={sponsor_logos}
               src={sponsor_logos}
               height={20}
               width={20}
@@ -174,7 +95,6 @@ export default function PastSponsors() {
               priority
             />
           </motion.div>
-          {/* </div> */}
         </BlueBorderSquareBox>
       </motion.div>
     </div>
