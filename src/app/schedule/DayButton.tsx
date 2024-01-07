968c60cf01c3e9ae74d1cd2f40695aa4fb842ce0
Mumtahin Farabi <mfarabi619@gmail.com>
2024-01-06 21:55:24 -0500

    feat: Improve DayButton component animations in schedule page

--- src/app/schedule/DayButton.tsx
+++ src/app/schedule/DayButton.tsx
@@ -13,8 +13,9 @@ export default function DayButton(props: DayButtonProps) {
   const bgColor = props.selected
     ? "bg-gradient-to-br from-royalPurple via-roseQuartz to-goldenApricot"
     : "bg-gray-200";
-  const textColor = props.selected ? "text-white" : "text-black";
-  const dotColor = props.selected ? "bg-white" : "bg-transparent";
+  const textColor = props.selected ? "text-transparent" : "text-black";
+  const dotColor = props.selected ? "bg-pink-50" : "bg-transparent";
+  const dayTextColor = props.selected ? "text-transparent" : "text-gray-600";
 
   return (
     <section
@@ -23,7 +24,7 @@ export default function DayButton(props: DayButtonProps) {
     >
       {/* Text for day of the conference, for example: 'Day One' */}
       <span
-        className={`text-[16px] md:text-[24px] font-semibold text-gray-600 transition-all ease-in-out duration-1000 text-center`}
+        className={`text-[16px] md:text-[24px] font-semibold ${dayTextColor} transition-all ease-in-out duration-1000 text-center bg-clip-text bg-gradient-to-r from-royalPurple via-roseQuartz to-goldenApricot`}
       >
         {props.titleText}
       </span>
@@ -33,12 +34,12 @@ export default function DayButton(props: DayButtonProps) {
         className={`flex flex-col items-center justify-center rounded-t-full rounded-b-full font-medium py-3 px-8 md:px-10 max-w-[60px] ${bgColor} transition-all ease-in-out duration-1000`}
       >
         <span
-          className={`text-[20px] md:text-[24px] ${textColor} transition-all ease-in-out duration-1000`}
+          className={`text-[20px] md:text-[24px] ${textColor} transition-all ease-in-out duration-1000 bg-clip-text bg-pink-50`}
         >
           {props.Day}
         </span>
         <span
-          className={`text-[24px] md:text-[32px] -mt-1 mb-1 ${textColor} transition-all ease-in-out duration-1000`}
+          className={`text-[24px] md:text-[32px] -mt-1 mb-1 ${textColor} transition-all ease-in-out duration-1000 bg-clip-text bg-pink-50`}
         >
           {props.Date}
         </span>
