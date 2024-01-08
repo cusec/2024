772fa680dc0fce202010ac2cc56f4a14747d3286
Mumtahin Farabi <mfarabi619@gmail.com>
2024-01-08 00:05:46 -0500

    chore: Add DaisyUI and PWA files

--- tailwind.config.js
+++ tailwind.config.js
@@ -36,5 +36,18 @@ module.exports = {
   },
   plugins: [
     require('tailwind-scrollbar')(),
+    require("daisyui")
   ],
+
+  // DaisyUI config
+  daisyui: {
+     themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: "light", // name of one of the included themes for dark mode
+    base: false, // applies background color and foreground color for root element by default
+    styled: false, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ":root", // The element that receives theme color CSS variables
+  }
 };
